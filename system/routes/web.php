1a24394cb0a71c42fa24229d15cf0b10b248ee94
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-06-07 16:36:02 +0700

    route

--- system/routes/web.php
+++ system/routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminBerandaController;
+use App\Http\Controllers\SuperadminBerandaController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function () {return view('login');});
+    
+
+Route::get('login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('login', [AuthController::class, 'loginProcess']);
+Route::get('logout', [AuthController::class, 'logout']);
+
+Route::prefix('admin')->middleware('admin')->group(function(){
+    Route::get('/', [AdminBerandaController::class, 'beranda']);
+    Route::get('beranda', [AdminBerandaController::class, 'beranda']);
+});
+
+Route::prefix('superadmin')->middleware('superadmin')->group(function(){
+    Route::get('/', [SuperadminBerandaController::class, 'beranda']);
+    Route::get('beranda', [SuperadminBerandaController::class, 'beranda']);
 });
