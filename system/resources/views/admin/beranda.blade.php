0e2880ecae6a1efc5dcdfd8bdea5bedae01e39cb
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-06-07 16:35:54 +0700

    view

---
+++ system/resources/views/admin/beranda.blade.php
@@ -0,0 +1,4 @@
+<a href="{{url('logout')}}" onclick="return confirm('Apakah Anda Yakin Ingin Keluar?')">Logout</a> <br>
+
+          
+beranda admin
---
+++ system/resources/views/superadmin/beranda.blade.php
@@ -0,0 +1,3 @@
+<a href="{{url('logout')}}" onclick="return confirm('Apakah Anda Yakin Ingin Keluar?')">Logout</a> <br>
+
+beranda superadmin
