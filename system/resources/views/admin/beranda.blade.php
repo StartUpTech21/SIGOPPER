a71d288299414a08a5a5f028ec839133fc87b650
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-07-06 12:02:52 +0700

    hapus sigopper lama

--- system/resources/views/admin/beranda.blade.php
+++
@@ -1,4 +0,0 @@
-<a href="{{url('logout')}}" onclick="return confirm('Apakah Anda Yakin Ingin Keluar?')">Logout</a> <br>
-
-          
-beranda admin
--- system/resources/views/template/section/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Sri Rahayu Ningsih
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2017 @if(date("Y") > '2017') - {{date("Y")}} @endif <a href="https://informatika.politap.ac.id">Teknik Informatika POLITAP</a>.</strong> All rights reserved.
-  </footer> 
